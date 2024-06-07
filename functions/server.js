fb12c48e662e3a825673895d8c1b17e0cab9327f
Gabriel MONTOIRO-VALERA <gmontoiro@cdg82.local>
2024-06-07 09:54:56 +0200

    changes

--- functions/server.js
+++ functions/server.js
@@ -7,81 +7,74 @@ const path = require('path');
 const app = express();
 const router = express.Router();
 
-// URL de conexión a MongoDB
 const url = 'mongodb+srv://userdb:abc123.@clustergalaxy.hdklzaq.mongodb.net/?retryWrites=true&w=majority';
 const dbName = 'galaxy';
 let db;
 
-// Configuración de MongoDB
-async function connectToDatabase() {
-    try {
-        const client = await MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
+MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(client => {
         db = client.db(dbName);
         console.log('Connected to database');
-    } catch (error) {
+    })
+    .catch(error => {
         console.error('Error connecting to database:', error);
         process.exit(1); // Exit if unable to connect to the database
-    }
-}
-
-// Llama a la función de conexión cuando la aplicación se inicia
-connectToDatabase();
+    });
 
 // Habilita CORS
 app.use(cors());
 
-// Define las rutas
 router.get("/", (req, res) => {
     res.send("App is running..");
 });
 
+// Endpoint para obtener los sistemas estelares
+router.get('/systems', (req, res) => {
+    const collection = db.collection('system');
+    collection.find({}).toArray()
+        .then(systems => res.json(systems))
+        .catch(error => res.status(500).send(error));
+});
+
+// Endpoint para obtener los planetas del sistema 'solar-cluster'
+router.get('/planets/solar-cluster', (req, res) => {
+    const collection = db.collection('planet');
+    collection.find({ system: 'solar-cluster' }).toArray()
+        .then(planets => res.json(planets))
+        .catch(error => res.status(500).send(error));
+});
+
+// Endpoint para obtener los planetas del sistema 'trebia-cluster'
+router.get('/planets/trebia-cluster', (req, res) => {
+    const collection = db.collection('planet');
+    collection.find({ system: 'trebia-cluster' }).toArray()
+        .then(planets => res.json(planets))
+        .catch(error => res.status(500).send(error));
+});
+
+// Endpoint para obtener los planetas del sistema 'citadel-cluster'
+router.get('/planets/citadel-cluster', (req, res) => {
+    const collection = db.collection('planet');
+    collection.find({ system: 'citadel-cluster' }).toArray()
+        .then(planets => res.json(planets))
+        .catch(error => res.status(500).send(error));
+});
+
+// Endpoint para cargar la página de inicio
 router.get('/gamestart', (req, res) => {
     res.sendFile(path.join(__dirname, 'gamestart.html'));
 });
 
-// Define las rutas para datos de la base de datos
-router.get('/systems', async (req, res) => {
-    try {
-        const collection = db.collection('system');
-        const systems = await collection.find({}).toArray();
-        res.json(systems);
-    } catch (error) {
-        res.status(500).send(error);
+// Define una ruta para redirigir a gamestart.html
+router.get("/redirect-to-gamestart", (req, res) => {
+    if (db) {
+        // Si la base de datos está conectada, redirige a gamestart.html
+        res.sendFile(path.join(__dirname, 'gamestart.html'));
+    } else {
+        res.status(500).send('Database connection not established.');
     }
 });
 
-router.get('/planets/solar-cluster', async (req, res) => {
-    try {
-        const collection = db.collection('planet');
-        const planets = await collection.find({ system: 'solar-cluster' }).toArray();
-        res.json(planets);
-    } catch (error) {
-        res.status(500).send(error);
-    }
-});
-
-router.get('/planets/trebia-cluster', async (req, res) => {
-    try {
-        const collection = db.collection('planet');
-        const planets = await collection.find({ system: 'trebia-cluster' }).toArray();
-        res.json(planets);
-    } catch (error) {
-        res.status(500).send(error);
-    }
-});
-
-router.get('/planets/citadel-cluster', async (req, res) => {
-    try {
-        const collection = db.collection('planet');
-        const planets = await collection.find({ system: 'citadel-cluster' }).toArray();
-        res.json(planets);
-    } catch (error) {
-        res.status(500).send(error);
-    }
-});
-
-// Usa el router en la ruta de funciones de Netlify
 app.use("/.netlify/functions/server", router);
 
-// Exporta la aplicación como una función serverless
 module.exports.handler = serverless(app);
